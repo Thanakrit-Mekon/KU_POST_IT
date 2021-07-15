97066c20c9fc50130d1192f808d8cfcc5f5e8398
PnPonPond <pnponpond@gmail.com>
2021-07-15 23:43:42 +0700

    add routing-PonPond

---
+++ frontend/src/App.tsx
@@ -0,0 +1,27 @@
+import Register from "./pages/Register";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/login"></Route>
+          <Route path="/sta"></Route>
+          <Route path="/project-coop"></Route>
+          <Route path="/internship"></Route>
+          <Route path="/posts/new"></Route>
+          <Route path="/posts"></Route>
+          <Route path="/posts/:postId"></Route>
+          <Route path="/posts/:postId/edit"></Route>
+          <Route path="/" exact></Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
