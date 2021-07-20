f2362387961dac47715f1138a4d9dd3689a6aa47
unknown <jinnajate.a@ku.th>
2021-07-20 22:29:20 +0700

    update database eng ver

---
+++ database_generator/faculty_table.py
@@ -0,0 +1,31 @@
+from pymongo import MongoClient
+from csv import reader
+
+
+client = MongoClient("mongodb://localhost:27017/")
+
+# database name
+mydb = client["ku_post_it_db"]
+
+# table name
+mycollection = mydb["faculty_table"]
+
+
+# read data from csv file
+with open("faculty_code.csv",  encoding="utf8") as read_obj:
+    csv_reader = reader(read_obj)
+    list_of_rows = list(csv_reader)
+
+# create insert_list
+insert_list = []  
+for row in list_of_rows:
+    insert_list.append({
+                        "faculty_code":row[0],
+                        "faculty_name":row[2]
+                        })
+
+# insert to mongodb
+x = mycollection.insert_many(insert_list)
+
+#print list of the _id values of the inserted documents:
+print(x.inserted_ids)
