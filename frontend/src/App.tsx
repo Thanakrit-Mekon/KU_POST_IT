d2866ab132e0ff479f226d2b66843389b82685ee
PnPonPond <pnponpond@gmail.com>
2021-07-23 22:37:48 +0700

    Create mypost -PP

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@material-ui/core/styles';
 import Register from './pages/Register';
 import CreatePost from './pages/CreatePost';
+import MyPost from './pages/MyPost';
 
 const theme = createTheme({
   palette: {
@@ -27,7 +28,9 @@ function App(): JSX.Element {
             <Route path="/posts/new">
               <CreatePost />
             </Route>
-            <Route path="/posts" />
+            <Route path="/posts">
+              <MyPost />
+            </Route>
             <Route path="/posts/:postId" />
             <Route path="/posts/:postId/table" />
             <Route path="/" exact />
---
+++ frontend/src/pages/MyPost.tsx
@@ -0,0 +1,91 @@
+import {
+  Container,
+  Typography,
+  Box,
+  Grid,
+  Card,
+  Avatar,
+  Button,
+} from "@material-ui/core";
+import NavBar from "../components/MyPost/NavBar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
+function MyPost() {
+  const data = [
+    {
+      id: "asdasdasd",
+      name: "Knun",
+      subject: "Sex education",
+      amount: 3,
+    },
+    {
+      id: "bbbbbbb",
+      name: "Soft",
+      subject: "Physic",
+      amount: 5,
+    },
+    {
+        id: "asdasdasd",
+        name: "Knun",
+        subject: "Sex education",
+        amount: 3,
+      },
+      {
+        id: "bbbbbbb",
+        name: "Soft",
+        subject: "Physic",
+        amount: 5,
+      },
+      {
+        id: "asdasdasd",
+        name: "Knun",
+        subject: "Sex education",
+        amount: 3,
+      },
+  ];
+
+  return (
+    <div>
+      <NavBar />
+      <Container maxWidth="md">
+        <Typography variant="h4">
+          <Box
+            fontWeight="bold"
+            color="primary.main"
+            style={{ marginTop: 50, marginBottom: 50 }}
+          >
+            My Post
+          </Box>
+        </Typography>
+        <Grid container spacing={5}>
+          {data.map((obj) => {
+            return (
+              <Grid item sm={4}>
+                <Card style={{ padding:20}}>
+                  <Grid container direction="column" alignItems="center">
+                      <Avatar />
+                      <Box>{obj.name}</Box>
+                      <Box>TA {obj.subject}</Box>
+                      {/* <Icon className="fa fa-user" /> */}
+                    <Box alignItems="center"><FontAwesomeIcon icon={faUser} />  {obj.amount} </Box>
+                    <Grid container justifyContent="space-between" style={{marginTop:20}}>
+                      <Button variant="contained" color="primary">
+                        View
+                      </Button>
+                      <Button variant="contained" color="secondary">
+                        Close
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </Card>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Container>
+    </div>
+  );
+}
+
+export default MyPost;
