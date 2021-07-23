65d73b4ca1cef31bf421e044762f7d3229ef4baf
PnPonPond <pnponpond@gmail.com>
2021-07-23 17:28:52 +0700

    asda

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,36 @@
-import CreatePost from "./pages/CreatePost";
-import { BrowserRouter, Switch, Route } from "react-router-dom"
-import { createTheme } from "@material-ui/core/styles";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import Register from './pages/Register';
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#5E9EA0",
+      main: '#5E9EA0',
     },
   },
 });
 
-function App() {
+function App(): JSX.Element {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Switch>
-          <Route path="/createpost">
-            <CreatePost />
-          </Route>
-          <Route path="/login"></Route>
-          <Route path="/sta"></Route>
-          <Route path="/project-coop"></Route>
-          <Route path="/internship"></Route>
-          <Route path="/posts/new"></Route>
-          <Route path="/posts"></Route>
-          <Route path="/posts/:postId"></Route>
-          <Route path="/posts/:postId/edit"></Route>
-          <Route path="/" exact></Route>
-        </Switch>
-      </div>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Switch>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/login" />
+            <Route path="/ta" />
+            <Route path="/project-coop" />
+            <Route path="/internship" />
+            <Route path="/posts/new" />
+            <Route path="/posts" />
+            <Route path="/posts/:postId" />
+            <Route path="/posts/:postId/edit" />
+            <Route path="/" exact />
+          </Switch>
+        </div>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
