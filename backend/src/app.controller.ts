72cd5eefad58a6a92cf240f129822550786731fb
unknown <jinnajate.a@ku.th>
2021-07-28 17:52:43 +0700

    use mongoose

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,23 +1,12 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AuthService } from './auth/auth.service';
-import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private authService: AuthService
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
-  async login(@Request() req) {
-    return this.authService.login(req.user);
   }
 }
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,51 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { TypeOrmModule } from '@nestjs/typeorm';
-
-import Faculty from './dropdowns/faculty.entity';
-import Department from './dropdowns/department.entity';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { DropdownsModule } from './dropdowns/dropdowns.module';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-
-import { MailModule } from './mail/mail.module';
-import { MailService } from './mail/mail.service';
-import { MailController } from './mail/mail.controller';
-
-import { ConfigModule } from '@nestjs/config';
-
-import { Posts } from './posts/posts.entity';
-import { Postscontroller } from './posts/posts.controller';
-import { PostsService } from './posts/posts.service';
 
 @Module({
   imports: [
-    // for Root to create build connection
-    TypeOrmModule.forRoot({
-      type: "mongodb",
-      host: "localhost",
-      database: "kupostit",
-      entities: [Posts, Faculty, Department],
-      synchronize: true,
-    }),
-
     DropdownsModule,
 
-    AuthModule,
-
-    UsersModule,
-
-    // for Feature for submodule
-    TypeOrmModule.forFeature([Posts]),
-
-    MailModule,
-    ConfigModule.forRoot({
-      isGlobal: true, 
-    }),
+    MongooseModule.forRoot(
+      'mongodb+srv://nonhsmile:1q2w3e4r@cluster0.ra2i1.mongodb.net/kupostit?retryWrites=true&w=majority',
+    ),
   ],
-  controllers: [AppController, MailController, Postscontroller],
-  providers: [AppService, MailService, PostsService], // Injectable
+  
+  controllers: [AppController,],
+  providers: [AppService,],
 })
 export class AppModule {}
