0e4e377e3bebe4b0f023b4d1e6dee20cc280c856
madtq <knun.amat@gmail.com>
2021-07-31 18:40:00 +0700

    add formik and yup - K

--- frontend/src/components/CreatePost/SelectedFrom.tsx
+++ frontend/src/components/CreatePost/SelectedFrom.tsx
@@ -1,10 +1,64 @@
 import { Grid, MenuItem, TextField } from "@material-ui/core";
+import axios from "axios";
 import { useFormik } from "formik";
+import { useEffect, useState } from "react";
 import * as yup from "yup";
 
-const faculties = ['1', '2', '3'];
+const validationSchema = yup.object({
+    title: yup.string().required(),
+    contact: yup.string().required(),
+    more: yup.string()
+});
 
-export default function SelectedFrom(): JSX.Element {
+interface Faculty {
+id: string;
+faculty_name: string;
+faculty_code: string;
+}
+
+interface Department {
+id: string;
+faculty_name: string;
+department_name: string;
+department_code: string;
+}  
+
+function SelectedFrom(): JSX.Element {
+    const [faculties, setFaculties] = useState<Faculty[]>([]);
+    const [departments, setDepartments] = useState<Department[]>([]);
+    
+    const formik = useFormik({
+        initialValues: {
+        title: "",
+        faculty: "",
+        department: "",
+        year: "",
+        contact: "",
+        number: "",
+        more: ""
+    },validationSchema,
+    onSubmit: (values) => {
+        const userData = {
+        };
+        console.log(userData);
+    },})
+
+    useEffect(() => {
+        axios.get("http://localhost:3000/dropdowns/faculties").then((response) => {
+        setFaculties(response.data);
+        });
+    }, []);
+
+    useEffect(() => {
+        axios
+        .get(
+            `http://localhost:3000/dropdowns/department/${formik.values.faculty}`
+        )
+        .then((response) => {
+            setDepartments(response.data);
+        });
+    }, [formik.values.faculty]);
+    
     return (
         <Grid container spacing={3}>
             <Grid item sm={6} style={{ marginBottom: '1rem' }}>
@@ -14,12 +68,13 @@ export default function SelectedFrom(): JSX.Element {
                 fullWidth
                 variant="outlined"
                 label="Faculty">
-                {faculties.map((faculty, index) => {
-                return (
-                    <MenuItem key={index} value={faculty}>
-                    {faculty}
-                    </MenuItem>
-                );
+                {faculties &&
+                  faculties.map((faculty) => {
+                    return (
+                      <MenuItem key={faculty.id} value={faculty.faculty_code}>
+                        {faculty.faculty_name}
+                      </MenuItem>
+                    );
                 })}
             </TextField>
             </Grid>
@@ -32,8 +87,8 @@ export default function SelectedFrom(): JSX.Element {
                 label="Branch">
                 {faculties.map((faculty, index) => {
                 return (
-                    <MenuItem key={index} value={faculty}>
-                    {faculty}
+                    <MenuItem key={index} value={formik.values.faculty}>
+                        {formik.values.faculty}
                     </MenuItem>
                 );
                 })}
@@ -48,8 +103,8 @@ export default function SelectedFrom(): JSX.Element {
                 label="Year">
                 {faculties.map((faculty, index) => {
                 return (
-                    <MenuItem key={index} value={faculty}>
-                    {faculty}
+                    <MenuItem key={index} value={formik.values.faculty}>
+                        {formik.values.faculty}
                     </MenuItem>
                 );
                 })}
@@ -59,3 +114,5 @@ export default function SelectedFrom(): JSX.Element {
 
     )
 }
+
+export default SelectedFrom
