99365021b048b0ea697485d88d4ea14a4123c24e
PnPonPond <pnponpond@gmail.com>
2021-09-11 19:41:46 +0700

    config axios-PP

--- frontend/src/axios.tsx
+++ frontend/src/axios.tsx
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "https://ku-post-it-backend.herokuapp.com/",
-  baseURL: "http://localhost:3000/",
+  // baseURL: "http://localhost:3000/",
+  baseURL: "http://139.59.244.209/",
   timeout: 20000,
   // baseURL: "https://1158-113-53-146-170.ngrok.io/",
   // timeout: 20000,
