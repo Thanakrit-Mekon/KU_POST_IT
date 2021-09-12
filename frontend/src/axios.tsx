ee8c0448694de3ed1d7d30dc037a18bf6fd61a1e
PnPonPond <pnponpond@gmail.com>
2021-09-12 22:32:54 +0700

    change backend url-PP

--- frontend/src/axios.tsx
+++ frontend/src/axios.tsx
@@ -3,10 +3,9 @@ import axios from "axios";
 const instance = axios.create({
   // baseURL: "https://ku-post-it-backend.herokuapp.com/",
   // baseURL: "http://localhost:3000/",
-  baseURL: "http://139.59.244.209/",
+  // baseURL: "http://139.59.244.209/",
+  baseURL: "https://kupostit.xyz/",
   timeout: 20000,
-  // baseURL: "https://1158-113-53-146-170.ngrok.io/",
-  // timeout: 20000,
 });
 
 export default instance;
